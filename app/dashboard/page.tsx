f2b7841d854ee16b6337ff0a8691f65a5a73d9ce
Kyle Fick <47220920+KyleFick@users.noreply.github.com>
2025-04-29 08:23:37 +0200

    Update page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+const Dashboard = () => {
+  return(
+    <div>
+      <div>
+        <h1>This is the Dashboard</h1>
+      </div>
+
+      <div>
+        This is where the content goes  
+      </div>
+    </div>
+  )
+}
